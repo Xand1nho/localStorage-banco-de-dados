41072b73e361259d30e779382608d02f82edef7d
Xandinho <souzasantosalexandre825@gmail.com>
2025-10-16 11:50:51 -0300

    Add files via upload

--- Loja/script.js
+++ Loja/script.js
@@ -1,7 +1,7 @@
 let produtos
 
 window.onload = function(){
-    var storedUser = localStorage.getItem("user")
+    var storedUser = localStorage.getItem("usuario")
     var user = JSON.parse(storedUser)
 
     var dataEntrada = new Date(user.dataEntrada)
@@ -11,8 +11,7 @@ window.onload = function(){
         month: "2-digit",
         year: "numeric",
         hour: "numeric",
-        minute: "numeric",
-        second: "numeric"
+        minute: "numeric"
     })
 
     document.getElementById("user").textContent = user.name
@@ -21,46 +20,54 @@ window.onload = function(){
 }
 
 document.addEventListener("DOMContentLoaded", function(){
-    fetch('../Dados/loja.json')
-    .then(response => response.json())
-    .then((data) => {
-        produtos = data
-        const produtosContainer = document.getElementById("produtos-container")
-        produtos.forEach(produtos, index) => {
-            const card = document.createElement("div")
-            card.className = "card"
-            card.style.width = "18rem"
+    fetch("../Dados/loja.json")
+        .then((response) => response.json())
+        .then((data) => {
+            produtos = data
+            const produtosContainer = document.getElementById("produtos-container")
+            produtos.forEach((produto, index) => {
+                const card = document.createElement("div")
+                card.className = "card"
+                card.style.width = "18rem"
 
-            const imagem = document.createElement("img")
-            imagem.src = produtos.imagem
-            imagem.className = "card-img-top"
+                const imagem = document.createElement("img")
+                imagem.src = produto.imagem
+                imagem.className = "card-img-top"
 
-            const cardBody = document.getElementById("div")
-            cardBody.className = "card-body"
+                const cardBody = document.createElement("div")
+                cardBody.className = "card-body"
 
-            const cardTitle = document.createElement("h5")
-            cardTitle.className = "card-title"
-            cardTitle.textContent = produto.desc
+                const cardTitle = document.createElement("h5")
+                cardTitle.className = "card-title"
+                cardTitle.textContent = produto.desc
 
-            const cardText = document.createElement("p")
-            cardText.className = "card-text"
-            cardText.textContent = "Preço: $" + produto.preco.toFixed(2)
+                const cardText = document.createElement("p")
+                cardText.className = "card-text"
+                cardText.textContent = "Preço: R$: " + produto.preco.toFixed(2)
 
-            const btnAdicionarAoCarrinho = document.createElement("a")
-            btnAdicionarAoCarrinho.href = "#"
-            btnAdicionarAoCarrinho.className = "btn btn-primary adicionar"
-            btnAdicionarAoCarrinho.textContent = "Adicionar ao carrinho"
-            btnAdicionarAoCarrinho.setAttribute("data-indice", index)
+                const btnAdicionarAoCarrinho = document.createElement("a")
+                btnAdicionarAoCarrinho.href = "#"
+                btnAdicionarAoCarrinho.className = "btn btn-primary adicionar"
+                btnAdicionarAoCarrinho.textContent = "Adicionar ao carrinho"
+                btnAdicionarAoCarrinho.setAttribute("data-indice", index)
 
-            cardBody.appendChild(cardTitle)
-            cardBody.appendChild(cardText)
-            cardBody.appendChild(btnAdicionarAoCarrinho)
+                cardBody.appendChild(cardTitle)
+                cardBody.appendChild(cardText)
+                cardBody.appendChild(btnAdicionarAoCarrinho)
 
-            card.appendChild(imagem)
-            card.appendChild(cardBody)
+                card.appendChild(imagem)
+                card.appendChild(cardBody)
 
-            produtosContainer.appendChild(card)
-        });
-        
-    }).catch((error) => console.log("Erro ao carregar dados", error))
+                produtosContainer.appendChild(card)
+            });
+        }).catch((error) => console.log("Erro ao carregar dados", error))
+
+        $("#produtos-container").on("click", ".adicionar", function(){
+            const indexDoProduto = $(this).data("indice")
+            const produtoSelecionado = produtos[indexDoProduto]
+            let carrinho = JSON.parse(localStorage.getItem("carrinho")) || []
+            carrinho.push(produtoSelecionado)
+            localStorage.setItem("carrinho", JSON.stringify(carrinho))
+            alert("Produto adicionado com sucesso!!")
+        })
 })
